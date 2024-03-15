9f51010ca652dd767ed16c57d24c743f8532e929
Zidane Arkan <zidark897@gmail.com>
2024-03-15 13:33:40 +0700

    Add SingleChildScrollvIEW

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,34 +7,39 @@ class QuestionsSummary extends StatelessWidget{
 
   @override
   Widget build(context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      children : summaryAnswers.map((data) {
-          return Row(children: [
-            Text(
-              ((data["question_index"] as int) + 1).toString(),
-            ),
-            // Restrict width of columns to match with width of row
-            Expanded(
-              child: Column(
-                children : [
-                  const SizedBox(height: 16,),
-                  Text(
-                    data["question"] as String,
-                    style: GoogleFonts.nunito(
-                      fontSize: 12,
-                      color: Colors.black
-                    ),
+    return SizedBox(
+      height: 500,
+      child: SingleChildScrollView(
+        child: Column(
+          children : summaryAnswers.map((data) {
+            return Row(
+              children: [
+                Text(
+                  ((data["question_index"] as int) + 1).toString(),
+                ),
+                // Restrict width of columns to match with width of row
+                Expanded(
+                  child: Column(
+                    children : [
+                      const SizedBox(height: 16,),
+                      Text(
+                        data["question"] as String,
+                        style: GoogleFonts.nunito(
+                          fontSize: 12,
+                          color: Colors.black
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      Text(data["correct_answer"] as String),
+                      const SizedBox(height: 5,),
+                      Text(data["user_answer"] as String),
+                    ],
                   ),
-                  const SizedBox(height: 10,),
-                  Text(data["correct_answer"] as String),
-                  const SizedBox(height: 5,),
-                  Text(data["user_answer"] as String),
-                ],
-              ),
-            ),
-          ],);
-      }).toList(),
+                ),
+            ],);
+          }).toList(),
+        ),
+      ),
     );
   }
 }
